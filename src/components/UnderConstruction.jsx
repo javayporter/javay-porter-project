700e4c0eb7df871cf23cc1ce99df7e740755a721
javayporter <javayscript@gmail.com>
2023-08-09 07:19:35 +0000

    update component

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -1,12 +1,12 @@
 const UnderConstruction = () => {
     return (
-        <div>
+        <div style={{padding: '3rem'}}>
             <h1>Page Under Constructon</h1>
             <h2>Content will be avaiable soon! Thank you for your patience.</h2>
-            <div className="subscribe"> 
+            {/* <div className="subscribe"> 
             <h3>For updates, click the button below!</h3>
             <button>Subscribe</button>
-            </div>
+            </div> */}
         </div>
     )
 }
